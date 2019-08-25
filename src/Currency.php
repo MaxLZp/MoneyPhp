c3852c10b72df60dd6c2d36de643dd569fd3d320
MaxLZp <lmax_job@mail.ru>
2019-08-25 09:00:46 +0300

    Currency implementation

---
+++ src/Currency.php
@@ -0,0 +1,100 @@
+<?php
+namespace MaxLZp\Money;
+
+/**
+ * Class Currency
+ * @package MaxLZp\Money
+ */
+class Currency implements ICurrency
+{
+    /**
+     * @var string Currency string-code
+     */
+    protected $code;
+    /**
+     * @var string Currency iso-code
+     */
+    protected $isoCode;
+    /**
+     * @var Currency name
+     */
+    protected $name;
+    /**
+     * Currency constructor.
+     * @param string $code
+     * @param string $name
+     * @param string $isoCode
+     */
+    public function __construct(
+        string $code,
+        string $name,
+        string $isoCode = '0'
+    ) {
+        $this->code = $code;
+        $this->isoCode = $isoCode;
+        $this->name = $name;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function equals(ICurrency $other): bool
+    {
+        return $this->codesAreEqual($other) &&
+            $this->isoCodesAreEqual($other);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getIsoCode(): string
+    {
+        return $this->isoCode;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString(): string
+    {
+        return $this->getCode();
+    }
+
+    /**
+     * @param ICurrency $other
+     * @return bool
+     */
+    protected function codesAreEqual(ICurrency $other): bool
+    {
+        return $this->stringsAreEqual($this->getCode(), $other->getCode());
+    }
+    /**
+     * @param ICurrency $other
+     * @return bool
+     */
+    protected function isoCodesAreEqual(ICurrency $other): bool
+    {
+        return $this->stringsAreEqual($this->getIsoCode(), $other->getIsoCode());
+    }
+    /**
+     * @param string $first
+     * @param string $second
+     * @return bool
+     */
+    protected function stringsAreEqual(string $first, string $second)
+    {
+        return \strcasecmp($first, $second) === 0;
+    }
+}
---
+++ tests/Currency/CurrencyTest.php
@@ -0,0 +1,130 @@
+<?php
+namespace maxlzp\money\tests;
+use MaxLZp\Money\Currency;
+
+/**
+ * Class CurrencyTest
+ * @package Tests
+ */
+class CurrencyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function Currency_constructor_createsInstanceWithDefaultIsoCodeSetToZero()
+    {
+        $expectedCode = 'USD';
+        $expectedName = 'US Dollar';
+        $expectedIsoCode = '0';
+        $currency = new Currency($expectedCode, $expectedName);
+
+        $this->assertNotNull($currency);
+        $this->assertEquals($expectedCode, $currency->getCode());
+        $this->assertEquals($expectedIsoCode, $currency->getIsoCode());
+        $this->assertEquals($expectedName, $currency->getName());
+    }
+    /**
+     * @test
+     */
+    public function Currency_constructor_createsInstanceWithIsoCode()
+    {
+        $expectedCode = 'USD';
+        $expectedName = 'US Dollar';
+        $expectedIsoCode = '840';
+        $currency = new Currency($expectedCode, $expectedName, $expectedIsoCode);
+
+        $this->assertNotNull($currency);
+        $this->assertEquals($expectedCode, $currency->getCode());
+        $this->assertEquals($expectedIsoCode, $currency->getIsoCode());
+        $this->assertEquals($expectedName, $currency->getName());
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_twoIdenticalCurrenciesAreEqual()
+    {
+        $code = 'USD';
+        $name = 'US Dollar';
+        $isoCode = '840';
+        $currency1 = new Currency($code, $name, $isoCode);
+        $currency2 = new Currency($code, $name, $isoCode);
+
+        $this->assertTrue($currency1->equals($currency2));
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_CurrenciesWithSameCodeButDifferentNamesAreEqual()
+    {
+        $code = 'USD';
+        $name1 = 'US Dollar';
+        $name2 = 'US Dollars';
+        $isoCode = '840';
+        $currency1 = new Currency($code, $name1, $isoCode);
+        $currency2 = new Currency($code, $name2, $isoCode);
+
+        $this->assertTrue($currency1->equals($currency2));
+        $currency1->getCode();
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_CurrenciesWithSameCodeInDifferentCasesAreEqual()
+    {
+        $code1 = 'USD';
+        $code2 = 'usd';
+        $name = 'US Dollar';
+        $isoCode = '840';
+        $currency1 = new Currency($code1, $name, $isoCode);
+        $currency2 = new Currency($code2, $name, $isoCode);
+
+        $this->assertTrue($currency1->equals($currency2));
+        $currency1->getCode();
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_DifferentCurrenciesAreDifferent()
+    {
+        $code1 = 'USD';
+        $code2 = 'EUR';
+        $name = 'US Dollar';
+        $isoCode1 = '840';
+        $isoCode2 = '978';
+        $currency1 = new Currency($code1, $name, $isoCode1);
+        $currency2 = new Currency($code2, $name, $isoCode2);
+
+        $this->assertFalse($currency1->equals($currency2));
+        $currency1->getCode();
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_CurrenciesWithDifferentCodeAreDifferent()
+    {
+        $code1 = 'USD';
+        $code2 = 'EUR';
+        $name = 'US Dollar';
+        $isoCode = '840';
+        $currency1 = new Currency($code1, $name, $isoCode);
+        $currency2 = new Currency($code2, $name, $isoCode);
+
+        $this->assertFalse($currency1->equals($currency2));
+        $currency1->getCode();
+    }
+    /**
+     * @test
+     */
+    public function Currency_equals_CurrenciesWithDifferentIsoCodeAreDifferent()
+    {
+        $code = 'USD';
+        $name = 'US Dollar';
+        $isoCode1 = '840';
+        $isoCode2 = '0';
+        $currency1 = new Currency($code, $name, $isoCode1);
+        $currency2 = new Currency($code, $name, $isoCode2);
+
+        $this->assertFalse($currency1->equals($currency2));
+        $currency1->getCode();
+    }
+}
